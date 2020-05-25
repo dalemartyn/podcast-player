640357b452d78b2e1371787fd257fe61c73f54dd
Dale <dalemartyn@gmail.com>
2020-05-25 10:45:26 +0100

    add design podcasts

--- tasks/get-initial-state.js
+++ tasks/get-initial-state.js
@@ -9,63 +9,78 @@ const categories = [
     title: "Web",
     slug: "web",
     podcasts: [
-      "https://rss.simplecast.com/podcasts/6265/rss",          // react podcast
-      "http://http203.googledevelopers.libsynpro.com/rss",     // http 203
-      "https://feeds.feedwrench.com/js-jabber.rss",            // javascript jabber
-      "https://gomakethings.com/podcast/feed.rss",             // vanilla js
-      "https://changelog.com/podcast/feed",                    // changelog
-      "https://anchor.fm/s/10dbd4bc/podcast/rss",              // svelte radio
-      "https://feeds.transistor.fm/maintainers-anonymous",     // maintainers anonymous
-      "https://pinecast.com/feed/ladybug-podcast",             // ladybug
-      "https://anchor.fm/s/cf40bd0/podcast/rss",               // fist & scripts
+      "https://rss.simplecast.com/podcasts/6265/rss",                // react podcast
+      "http://http203.googledevelopers.libsynpro.com/rss",           // http 203
+      "https://feeds.feedwrench.com/js-jabber.rss",                  // javascript jabber
+      "https://gomakethings.com/podcast/feed.rss",                   // vanilla js
+      "https://changelog.com/podcast/feed",                          // changelog
+      "https://anchor.fm/s/10dbd4bc/podcast/rss",                    // svelte radio
+      "https://feeds.transistor.fm/maintainers-anonymous",           // maintainers anonymous
+      "https://pinecast.com/feed/ladybug-podcast",                   // ladybug
+      "https://anchor.fm/s/cf40bd0/podcast/rss",                     // fist & scripts
     ]
   },
   {
     title: "Business",
     slug: "business",
     podcasts: [
-      "http://exponent.fm/feed/",                              // exponent
-      "https://rss.art19.com/freakonomics-radio",              // feakonomics
-      "https://feeds.simplecast.com/JGE3yC0V",                 // a16z
-      "https://feeds.megaphone.fm/recodedecode",               // recode decode
-      "https://rss.art19.com/tim-ferriss-show",                // tim ferriss
-      "https://feeds.npr.org/510313/podcast.xml",              // how i built this
-      "https://feeds.transistor.fm/rework",                    // rework
-      "https://sivers.org/podcast.rss",                        // derek sivers
-      "https://naval.libsyn.com/rss",                          // naval
+      "http://exponent.fm/feed/",                                    // exponent
+      "https://rss.art19.com/freakonomics-radio",                    // feakonomics
+      "https://feeds.simplecast.com/JGE3yC0V",                       // a16z
+      "https://feeds.megaphone.fm/recodedecode",                     // recode decode
+      "https://rss.art19.com/tim-ferriss-show",                      // tim ferriss
+      "https://feeds.npr.org/510313/podcast.xml",                    // how i built this
+      "https://feeds.transistor.fm/rework",                          // rework
+      "https://sivers.org/podcast.rss",                              // derek sivers
+      "https://naval.libsyn.com/rss",                                // naval
+    ]
+  },
+  {
+    title: "Design",
+    slug: "design",
+    podcasts: [
+      "https://rss.simplecast.com/podcasts/1034/rss",                // design details
+      "https://feeds.99percentinvisible.org/99percentinvisible",     // 99% invisible
+      "https://designobserver.com/show.designmatters2009-10.xml",    // design matters
+      "https://www.relay.fm/presentable/feed",                       // presentable
+      "http://googledesignmethod.libsyn.com/rss",                    // method podcast
+      "http://designnotes.fm/rss",                                   // design notes
+      "https://feeds.simplecast.com/4MvgQ73R",                       // ui breakfast
+      "http://feeds.feedburner.com/uxpodcast",                       // ux podcast
+      "https://feeds.pacific-content.com/wireframe",                 // wireframe
     ]
   },
   {
     title: "Technology",
     slug: "technology",
     podcasts: [
-      "https://feeds.megaphone.fm/vergecast",                  // vergecast
-      "http://feeds.feedburner.com/WaveformWithMkbhd",         // waveform
-      "https://feeds.megaphone.fm/gadget-lab",                 // gadget lab
-      "https://daringfireball.net/thetalkshow/rss",            // the talk show daring fireball
-      "https://feeds.theoutline.com/tomorrow",                 // tomorrow
+      "https://feeds.megaphone.fm/vergecast",                        // vergecast
+      "http://feeds.feedburner.com/WaveformWithMkbhd",               // waveform
+      "https://feeds.megaphone.fm/gadget-lab",                       // gadget lab
+      "https://daringfireball.net/thetalkshow/rss",                  // the talk show daring fireball
+      "https://feeds.theoutline.com/tomorrow",                       // tomorrow
     ]
   },
   {
     title: "Football",
     slug: "football",
     podcasts: [
-      "https://audioboom.com/channels/2402227.rss",            // the anfield wrap
-      "https://audioboom.com/channels/5005036.rss",            // jamie carragher
-      "https://www.spreaker.com/show/3382900/episodes/feed",   // gary neville
-      "https://podcasts.files.bbci.co.uk/p06kyljg.rss",        // peter crouch
-      "https://rss.acast.com/footballweekly",                  // guardian football weekly
-      "https://www.spreaker.com/show/3382902/episodes/feed",   // sky sports
-      "https://rss.acast.com/quicklykevin",                    // quickly kevin
-      "https://rss.acast.com/athleticomince",                  // athletico mince
-      "https://feeds.simplecast.com/_cY16e7I",                 // red agenda
-      "https://podcast.global.com/show/4329967/episodes/feed", // blood red
+      "https://audioboom.com/channels/2402227.rss",                  // the anfield wrap
+      "https://audioboom.com/channels/5005036.rss",                  // jamie carragher
+      "https://www.spreaker.com/show/3382900/episodes/feed",         // gary neville
+      "https://podcasts.files.bbci.co.uk/p06kyljg.rss",              // peter crouch
+      "https://rss.acast.com/footballweekly",                        // guardian football weekly
+      "https://www.spreaker.com/show/3382902/episodes/feed",         // sky sports
+      "https://rss.acast.com/quicklykevin",                          // quickly kevin
+      "https://rss.acast.com/athleticomince",                        // athletico mince
+      "https://feeds.simplecast.com/_cY16e7I",                       // red agenda
+      "https://podcast.global.com/show/4329967/episodes/feed",       // blood red
     ]
   }
 ];
 
 const extraPodcasts = [
-  "https://podcasts.files.bbci.co.uk/p0742833.rss",            // bbc obsessed with
+  "https://podcasts.files.bbci.co.uk/p0742833.rss",                  // bbc obsessed with
 ];
 
 async function getPodcast(url) {
